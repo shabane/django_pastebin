bce51cf9d1880c24862a1354265309d952eaddc7
shabane <m.mohamadshabane@gmail.com>
2021-09-29 00:38:25 +0330

    added a random block-qoute color

--- clipboard/views.py
+++ clipboard/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+import random
 
 # Create your views here.
 
 def index(request):
+
+    colors = [
+        '#335c67;',
+        '#01957d',
+        '#536c69',
+        '#172e1a',
+        '#4ac75',
+        '#8c432a',
+        '#610094',
+    ]
+
     text = """
 [Hi, this site is a online clipboard manager],
 [Thease line on the head of your clipboard will help you to decide what to do],
@@ -12,7 +23,12 @@ def index(request):
 [by clicking on 'trash' icon delete a text from clipboard manager],
 [share a clipboard text by click on the 'share' button],
 """
-    return render(request, 'index.html', {'text': text })
+
+    return render(request, 'index.html', 
+    {
+        'text': text,
+        'qcolor': colors[random.randint(0, len(colors)-1)]
+    })
 
 
 def new(request):
