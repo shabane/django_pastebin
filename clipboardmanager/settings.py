fafdf742ba790b2132757a50c1c57655a8cd18d2
shabane <m.mohamadshabane@gmail.com>
2021-09-29 01:43:39 +0330

    login page created. home page now know hwo logged in.

--- clipboardmanager/settings.py
+++ clipboardmanager/settings.py
@@ -67,6 +67,7 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
             ],
         },
+        'DIRS': [str(BASE_DIR.joinpath('templates'))]
     },
 ]
 
@@ -130,3 +131,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = '/'
--- clipboardmanager/urls.py
+++ clipboardmanager/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('clipboard.urls'))
+    path('', include('clipboard.urls')),
+    path('accounts/', include('django.contrib.auth.urls'))
 ]
