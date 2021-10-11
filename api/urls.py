d1527bbdb33bdcc459e1b9c658d73af0f2643656
shabane <m.mohamadshabane@gmail.com>
2021-10-11 19:49:24 +0330

    api delete a text added

--- api/urls.py
+++ api/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('addtext', views.AddText)
+    path('addtext', views.AddText),
+    path('delete', views.DeleteText),
+    path('login', views.UserLogin)
 ]
--- api/views.py
+++ api/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from clipboard.models import User, Clipboard
 from django.contrib.auth.hashers import check_password
 from django.http import HttpResponse
+from clipboard import views as utils
 # Create your views here.
 
 def AddText(request):
@@ -17,3 +18,37 @@ def AddText(request):
             print('incorrect username or password')
     else:
         print('incorrect username or password')
+
+
+def UserLogin(request):
+    user = request.GET['user']
+    password = request.GET['password']
+    user = User.objects.filter(username=user)
+    if(user):
+        if(check_password(password, user[0].password)):
+            return HttpResponse('username and password is correct') # temp
+        else:
+            return HttpResponse('username or password incorrect')
+    else:
+        return HttpResponse('username or password incorrect')
+
+
+def DeleteText(request):
+    user = request.GET['user']
+    password = request.GET['password']
+    textId = request.GET['id']
+    user = User.objects.filter(username=user)
+    if(user):
+        if(check_password(password, user[0].password)):
+            if(Clipboard.objects.filter(id=textId, author=user[0]).delete()[0] == 1):
+                return HttpResponse('object deleted') # temp
+            else:
+                return HttpResponse('object not found')
+        else:
+            return HttpResponse('username or password incorrect')
+    else:
+        return HttpResponse('username or password incorrect')
+
+
+##### todo
+
